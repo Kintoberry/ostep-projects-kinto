f3b53e4d6a38adc5b1706997c7612535e06da987
Hansu Lee <kintoberry@gmail.com>
2023-03-28 14:42:32 +0900

    implement check_executable_path_validity function

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -10,18 +10,42 @@ size_t PATH_LEN = 0;
 
 char* process_input(FILE* f_source);
 int init_path_variable(char** PATH);
-int check_executable_path(char* executable_path, char* PATH);
+int check_executable_path_validty(const char* executable_path, const char** pPATH);
+bool is_absolute_path(const char* executable_path);
 
+bool is_absolute_path(const char* executable_path) {
+    // assume Linux path
+    char *path_separator = "/";
+    if (executable_path[0] == *path_separator) {
+        return true;
+    }
+    return false;
+}
 
-int check_executable_path(char* executable_path, char* PATH) {
+const char* check_executable_path_validity(const char* executable_path, const char* PATH) {
+    // immutability is important
+    if (is_absolute_path(executable_path)) {
+        if (access(executable_path, X_OK) == 0) {
+            return executable_path;
+        }
+    }
+    // only binary filename is given
+    char* PATH_copy = strdup(PATH);
     const char* delimiter = ":";
-    // char* each_path;
-    // while ((each_path = strsep(&input, delimiter)) != NULL) {
-    //         printf("inputs: %s\n", token);
-    //     }
-    // if (access(executable_path, X_OK) == -1) {
-    //     return -1;
-    // }    
+    char* path;
+    char* absolute_executable_path;
+    // assume Linux path
+    const char *path_separator = "/";
+    while ((path = strsep(&PATH_copy, delimiter)) != NULL) {
+        absolute_executable_path = (char *) malloc(strlen(path) + strlen(path_separator) + strlen(executable_path) + strlen("\0"));
+        strcpy(absolute_executable_path, path);
+        strcat(absolute_executable_path, path_separator);
+        strcat(absolute_executable_path, executable_path);
+        if (access(absolute_executable_path, X_OK) == 0) {
+            return absolute_executable_path;
+        }  
+    }
+    return NULL;
 
 }
 
@@ -52,17 +76,19 @@ int main(int argc, char** argv) {
         }
         const char* delimiter = " ";
         char* token;
-        token = strsep(&input, delimiter);
-        if (token == NULL) {
+        // get the executable bin name, which is the first token
+        const char* executable_path = strsep(&input, delimiter);
+        if (executable_path == NULL) {
             printf("ERROR: no name of an executable file.\n");
             exit(1);
         }
-        // loop to check if this executable is okay within the known PATH
-        if (check_executable_path() == -1) {
+
+        const char *absolute_executable_path = check_executable_path_validity(executable_path, PATH);
+        // printf("absolute_executable: %s\n", absolute_executable_path);
+        if (absolute_executable_path == NULL) {
             printf("ERROR: cannot access the executable.\n");
             exit(1);
         }
-        
         
         while ((token = strsep(&input, delimiter)) != NULL) {
             printf("inputs: %s\n", token);
@@ -84,18 +110,17 @@ char* process_input(FILE* f_source) {
         printf("ERROR: file error from stdin.\n");
         return NULL;
     }
-    // printf("%s", input);
-    // immutability is important
     // remove newline character in the input
     if (input[nread - 1] == '\n') {
         input[nread - 1] = '\0';
     }
+    // immutability is important
     char* input_copy = strdup(input);
     return input_copy;
 }
 
 int init_path_variable(char** PATH) {
-    char* initial_path = "/bin:";
+    char* initial_path = "/bin:/usr/bin";
     *PATH = (char*) malloc(strlen(initial_path) + 1);
     if (*PATH == NULL) {
         return -1;
