6104150ca54b09bdcd3a55bcb08605c0120fda10
Hansu Lee <kintoberry@gmail.com>
2023-04-09 15:46:50 +0900

    Make one while loop to deal with either batch more or interative mode

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -13,41 +13,42 @@ int main(int argc, char** argv) {
         printf("ERROR: Bad usage.\n");
         exit(1);
     }
+    bool is_batch_mode = false;
+    if (argc == 2) {
+        is_batch_mode = true;
+    }
     int err = init_path();
     if (err == -1) {
         printf("ERROR: PATH initionalization failed.\n");
         exit(1);
     }
     
-    if (argc == 2) {
-        // batch mode
-        FILE* fp = fopen(argv[1], "r");
-        char *input_from_batch;
-        while ((input_from_batch = receive_input(fp)) != NULL) {
-            execute_input(input_from_batch);
-
-        }
-        fclose(fp);
+    // if (argc == 2) {
+    //     // batch mode
+    //     FILE* fp = fopen(argv[1], "r");
+    //     char *input_from_batch;
+    //     while ((input_from_batch = receive_input(fp)) != NULL) {
+    //         execute_input(input_from_batch);
+    //     }
+    //     fclose(fp);
+    // }
+    FILE *fp;
+    if (is_batch_mode) {
+        fp = fopen(argv[1], "r");  
+    } else {
+        fp = stdout;
     }
-    
-    while(true) {
-        printf("wish> ");
-        char* input = receive_input(stdin);
+    char* input;
+    while((input = receive_input(fp)) != NULL) {
+        if (!is_batch_mode)
+            printf("wish> ");
         if (input == NULL) {
             printf("ERROR: couldn't read the input.\n");
         }
         execute_input(input);
-
-        // char *absolute_executable_path = check_executable_path_validity(executable_path, PATH);
-        // // printf("absolute_executable: %s\n", absolute_executable_path);
-        // if (absolute_executable_path == NULL) {
-        //     printf("ERROR: cannot access the executable.\n");
-        //     // exit(1);
-        //     free(input_original);
-        //     continue;
-        // }
-        
-
+    }
+    if (fp != stdout) {
+        fclose(fp);
     }
 }
 
